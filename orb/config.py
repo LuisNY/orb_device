4a6e93464c3bdbd77eb7544328fd27a4548f81e2
Luigi Pepe <luigipepe@Luigis-MacBook-Pro-2.local>
2023-02-04 11:04:12 -0500

    fixed address issue

--- orb/config.py
+++ orb/config.py
@@ -1,6 +1,6 @@
 # below are all the variables that can be configured for testing purposes
 
-BASE_URL = 'http://0.0.0.0:8002'
+BASE_URL = ' http://mock_server:8002'  # change this to whatever IP address the target server might be
 IRISES_PATH = './irises/'
 
 SIGNUP_URL = BASE_URL + '/signup'
@@ -9,10 +9,10 @@ REPORT_STATUS_URL = BASE_URL + '/status'
 STATUS_REPORT_INTERVAL = 3  # secs
 LIFE_EVENT_INTERVAL = 5  # secs
 
-BATTERY_UPDATE_DELTA = -2  # secs
-CPU_TEMP_DELTA = 1  # secs
-CPU_USAGE_DELTA = 2  # secs
-DISK_SPACE_DELTA = -2  # secs
+BATTERY_UPDATE_DELTA = -2  # %
+CPU_TEMP_DELTA = 1  # Celsius
+CPU_USAGE_DELTA = 2  # %
+DISK_SPACE_DELTA = -2  # %
 
 BATTERY_RESET_VALUE = 100  # %
 CPU_TEMP_RESET_VALUE = 20  # Celsius
